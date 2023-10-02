1582f7be44edfe68eb9ee0a462093b94b749fe04
georghiton-luiz <georghiton@gmail.com>
2023-10-02 17:03:59 -0300

    Criacao da validacao de autenticacao, registro do usuario autenticado e middleware de upload de imagens

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -2,13 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 //Controller
-const { register } = require("../controllers/UserController");
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require("../controllers/UserController");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation");
-const { userCreateValidation } = require("../middlewares/useValidations");
+const {
+  userCreateValidation,
+  loginValidation,
+} = require("../middlewares/useValidations");
+const authGuard = require("../middlewares/authGuard");
 
 //Routes
 router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
 
 module.exports = router;
